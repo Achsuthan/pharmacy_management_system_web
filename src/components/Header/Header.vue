2d16671272234cca082b18e91f68084eb781880f
Achsuthan <achsuthan@icloud.com>
2019-10-28 22:56:21 +0530

    1. Pharmacy name added 2. Logout added
    >>> ach

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -4,11 +4,12 @@
       <b-link class="navbar-brand" to="/home">
         PMS
       </b-link>
-        Pharmacy Name
+        {{pharmacyName}}
       <b-navbar-nav class="d-md-down-none">
         <b-nav-item class="px-3" to="/home">Home</b-nav-item>
         <b-nav-item class="px-3" to="/orders" exact>Medicine Orders</b-nav-item>
         <b-nav-item class="px-3" to="/deliver">Deliver Medicine</b-nav-item>
+        <b-nav-item class="px-3" @click="logoutFn">Logout</b-nav-item>
       </b-navbar-nav>
     </AppHeader>
 </template>
@@ -18,8 +19,23 @@ import { Header as AppHeader } from '@coreui/vue'
 export default {
   name: 'DefaultHeader',
   components: {
-    
     AppHeader
+  },
+  data(){
+    return {
+    };
+  },
+  computed:{
+    pharmacyName(){
+      return this.$store.getters.getPharmacyName
+    }
+  },
+  methods:{
+    logoutFn(){
+      localStorage.clear()
+      this.$store.dispatch("setUser", null)
+      this.$router.push("/")
+    }
   }
 }
 </script>
