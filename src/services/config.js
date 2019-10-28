9aa53da23931f1b3f43f55fed9c152adb002dd7d
Achsuthan <achsuthan@icloud.com>
2019-10-28 22:57:16 +0530

    1. check user  function added with localstorage
    >>> ach

--- src/services/config.js
+++ src/services/config.js
@@ -8,7 +8,18 @@ else {
     baseUrl = "http://localhost:8888/pharmacy_management_system_backend"
 }
 
+const checkUser = ()=>{
+    let user = JSON.parse(localStorage.getItem("user"))
+    if(user){
+        return true
+    }
+    else {
+        return false
+    }
+}
+
 export default{
     isLive: isLive,
     baseUrl: baseUrl,
+    checkUser: checkUser,
 }
