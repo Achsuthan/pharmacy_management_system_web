b20bcf3f1140ade7900adf0aa5cfe35a57b81baa
Achsuthan <achsuthan@icloud.com>
2019-10-28 15:29:43 +0530

    1. User object added
    >>> ach

--- src/store.js
+++ src/store.js
@@ -6,22 +6,32 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state:{
-        isLoading: false
+        isLoading: false,
+        user: null,
 
     },
     mutations:{
         SET_LOADING(state,payload){
             state.isLoading = payload
+        },
+        SET_USER(state,payload){
+            state.user = payload
         }
     },
     actions:{
         setLoading({commit},payload){
             commit("SET_LOADING", payload)
+        },
+        setUser({commit},payload){
+            commit("SET_USER",payload)
         }
     },
     getters:{
         getLoading(state){
             return state.isLoading
+        },
+        getUser(state){
+            return state.user
         }
     }
 })
