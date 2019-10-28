ceb7450c11cd092ab920fc469772dbcf15a69b4e
Achsuthan <achsuthan@icloud.com>
2019-10-29 00:47:02 +0530

    1. Validate QR function added
    >>> ach

--- src/components/Delivery/Table.vue
+++ src/components/Delivery/Table.vue
@@ -16,7 +16,7 @@
       @row-clicked="rowClicked"
     >
       <template slot="Status" slot-scope="data">
-        <b-button size="sm" class="mr-2" @click="largeModal = true">{{data.item.Status}}</b-button>
+        <b-button size="sm" class="mr-2" @click="openQR(data.item)">{{data.item.Status}}</b-button>
       </template>
     </b-table>
     <nav>
@@ -29,7 +29,7 @@
         hide-goto-end-buttons
       />
     </nav>
-    <b-modal title="Scan QR" v-model="largeModal" @ok="largeModal = false">
+    <b-modal title="Scan QR" hide-footer v-model="largeModal" @ok="largeModal = false">
       <p class="error">{{ error }}</p>
 
       <p class="decode-result">
@@ -44,6 +44,8 @@
 
 <script>
 import { QrcodeStream } from "vue-qrcode-reader";
+import messageHandler from "../../handler/messageHandler";
+import qrService from "../../services/qr.service";
 
 export default {
   name: "c-table",
@@ -98,7 +100,8 @@ export default {
       currentPage: 1,
       largeModal: false,
       result: "",
-      error: ""
+      error: "",
+      selectedItem: null
     };
   },
   computed: {
@@ -111,9 +114,76 @@ export default {
     },
     captions: function() {
       return this.fields;
+    },
+    user() {
+      return this.$store.getters.getUser;
+    }
+  },
+  watch: {
+    result(val) {
+      console.log(val);
+      let tmp = new URL(val);
+      console.log(tmp.search);
+      var urlParams = new URLSearchParams(tmp.search);
+      let userId = urlParams.getAll("userId");
+      let prescriptionId = urlParams.getAll("prescriptionId");
+      let pharmacyId = urlParams.getAll("pharmacyId");
+      console.log(userId);
+      console.log(prescriptionId);
+      console.log(pharmacyId);
+      console.log(this.user.id)
+      console.log(this.selectedItem.user_id)
+
+      if (
+        userId.length > 0 &&
+        prescriptionId.length > 0 &&
+        pharmacyId.length > 0
+      ) {
+        if (this.user.id == pharmacyId[0] && this.selectedItem.user_id == userId[0]) {
+          console.log("Everything Ok");
+          this.$store.dispatch("setLoading", true);
+
+          let obj = {
+            userId: userId[0],
+            prescriptionId: prescriptionId[0],
+            pharmacyId: pharmacyId[0]
+          };
+          qrService
+            .validateQR(obj)
+            .then(res => {
+              if (res.data.status == 200) {
+                console.log("success");
+                this.$store.dispatch("setLoading", false);
+                this.largeModal = false
+                let obj2 = {
+                  pharmacyId: this.user.id
+                };
+                this.$store.dispatch("getPrescriptionForDeliver", obj2);
+              } else {
+                this.$store.dispatch("setLoading", false);
+                messageHandler.errorMessage("Failed", res.data.message);
+              }
+            })
+            .catch(error => {
+              this.$store.dispatch("setLoading", false);
+              console.log(error);
+              messageHandler.networkError();
+            });
+        } else {
+          console.log("error");
+          messageHandler.errorMessage("Failed", "Wrong QR");
+        }
+      } else {
+        console.log("error2");
+        messageHandler.errorMessage("Failed", "Wrong QR");
+      }
     }
   },
   methods: {
+    openQR(item){
+      this.selectedItem = item
+      this.largeModal = true
+    },
     onDecode(result) {
       this.result = result;
     },
