5b6ae3d673cb3f6864d790c7278a5ab635507075
Achsuthan <achsuthan@icloud.com>
2019-10-28 15:29:31 +0530

    1. Local storage added to store
    >>> ach

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <Header></Header>
     <router-view></router-view>
-     <loading
+    <loading
       :active.sync="isLoading"
       :can-cancel="false"
       :is-full-page="fullPage"
@@ -38,24 +38,30 @@ export default {
     isLoading(){
       return this.$store.getters.getLoading
     }
+  },
+  beforeMount(){
+    this.$store.dispatch(
+      "setUser",
+      JSON.parse(localStorage.getItem("user"))
+    );
   }
 }
 </script>
 
 <style lang="scss">
-  // CoreUI Icons Set
-  @import '~@coreui/icons/css/coreui-icons.min.css';
-  /* Import Font Awesome Icons Set */
-  $fa-font-path: '~font-awesome/fonts/';
-  @import '~font-awesome/scss/font-awesome.scss';
-  /* Import Simple Line Icons Set */
-  $simple-line-font-path: '~simple-line-icons/fonts/';
-  @import '~simple-line-icons/scss/simple-line-icons.scss';
-  /* Import Flag Icons Set */
-  @import '~flag-icon-css/css/flag-icon.min.css';
-  /* Import Bootstrap Vue Styles */
-  @import '~bootstrap-vue/dist/bootstrap-vue.css';
-  // Import Main styles for this application
-  @import 'assets/scss/style';
+// CoreUI Icons Set
+@import "~@coreui/icons/css/coreui-icons.min.css";
+/* Import Font Awesome Icons Set */
+$fa-font-path: "~font-awesome/fonts/";
+@import "~font-awesome/scss/font-awesome.scss";
+/* Import Simple Line Icons Set */
+$simple-line-font-path: "~simple-line-icons/fonts/";
+@import "~simple-line-icons/scss/simple-line-icons.scss";
+/* Import Flag Icons Set */
+@import "~flag-icon-css/css/flag-icon.min.css";
+/* Import Bootstrap Vue Styles */
+@import "~bootstrap-vue/dist/bootstrap-vue.css";
+// Import Main styles for this application
+@import "assets/scss/style";
 </style>
 
